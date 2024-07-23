7f91c5f69d4577324f69a6d58bd81c84ab9eed7b
Samuel Ogboye <ogboyesam@gmail.com>
2024-07-23 22:53:27 +0100

    feat: Updated user app with register and login

---
+++ users/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ['id', 'username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = get_user_model().objects.create_user(**validated_data)
+        return user
--- users/views.py
+++ users/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import UserSerializer
 
-# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]
+
+class LoginView(TokenObtainPairView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = TokenObtainPairSerializer
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+    
+class ProfileView(generics.RetrieveAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user

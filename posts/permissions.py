48e962e76ca5d32bddecb0c97ff05c7010b14495
Samuel Ogboye <ogboyesam@gmail.com>
2024-07-24 23:52:24 +0100

    refact: updated permision for post and comments

--- posts/permissions.py
+++ posts/permissions.py
@@ -4,4 +4,4 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return obj.authorId == request.user
+        return obj.author == request.user

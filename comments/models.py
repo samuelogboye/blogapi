bcd0f26a8db7e43187726f55ce8026cdf183c992
Samuel Ogboye <ogboyesam@gmail.com>
2024-07-24 23:51:36 +0100

    refact: updated comments model

--- comments/models.py
+++ comments/models.py
@@ -3,8 +3,8 @@ from django.conf import settings
 from posts.models import Post
 
 class Comment(models.Model):
-    postId = models.ForeignKey(Post, on_delete=models.CASCADE)
-    authorId = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     content = models.TextField()
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now=True)

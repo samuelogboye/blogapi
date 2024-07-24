98dc67a8d5eb72ff466101936f617f4d69234379
Samuel Ogboye <ogboyesam@gmail.com>
2024-07-24 23:51:06 +0100

    feat: updated the router link

--- blogapi/urls.py
+++ blogapi/urls.py
@@ -4,9 +4,16 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/users/', include('users.urls')),
-    path('api/posts/', include('posts.urls')),
-    path('api/comments/', include('comments.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path(
+        "api/",
+        include(
+            [
+                path('users/', include('users.urls')),
+                path('', include('posts.urls')),
+                path('', include('comments.urls')),
+                path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+                path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+            ]
+        ),
+    ),
 ]

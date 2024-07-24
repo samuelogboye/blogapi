856eb1c3ddad894a4c116c82a55acb460673e06d
Samuel Ogboye <ogboyesam@gmail.com>
2024-07-24 23:51:51 +0100

    refact: updated posts model

--- posts/models.py
+++ posts/models.py
@@ -4,6 +4,6 @@ from django.conf import settings
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
-    authorId = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now=True)

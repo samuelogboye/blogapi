1f6c46e38051104758bfcb6d9e2239f0c4703bd2
Samuel Ogboye <ogboyesam@gmail.com>
2024-07-26 19:27:23 +0100

    fix: updated secret key

--- blogapi/settings.py
+++ blogapi/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY=config('SECRET_KEY')
+SECRET_KEY=config('SECRET_KEY', "ok")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

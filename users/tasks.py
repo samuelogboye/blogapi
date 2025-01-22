c9c6f09e5e72457ce07ffaba12880e1391b82ba4
Samuel Ogboye <ogboyesam@gmail.com>
2025-01-22 22:58:32 +0100

    fix: update celery task for user app

--- users/tasks.py
+++ users/tasks.py
@@ -1,15 +1,25 @@
 from celery import shared_task
-from django.core.mail import send_mail
+# from django.core.mail import send_mail
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
 
 @shared_task
-def send_registration_email(subject, message, recipient_list):
+def send_notification_via_email(subject, recipient_list, template, context=None):
     """
-    Celery task to send registration email.
+    Celery task to send login notification via email.
     """
-    send_mail(
-        subject,
-        message,
-        'your_email@example.com',  # From email
-        recipient_list,  # To email
-        fail_silently=False,
+    if context is None:
+        context = {}
+
+   # Render the HTML content (this must be a string)
+    html_content = render_to_string(f'emails/{template}', context)
+
+    # Create the email object
+    email = EmailMessage(
+        subject=subject,               # Email subject
+        body=html_content,            # Email body (HTML content as string)
+        from_email='your_email@example.com',  # Replace with your sender email
+        to=recipient_list             # Recipient list
     )
+    email.content_subtype = "html"  # Specify the email content type as HTML
+    email.send()
